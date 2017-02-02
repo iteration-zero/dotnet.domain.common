8c546c896c9b6144139ee1e9e3ba423a429f3542
David Palumbo <david.palumbo@hotmail.com>
2017-02-02 00:17:48 -0500

    Add solution structure.

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ev
 
-dotnet restore code/IterationZero.Domain.Common/IterationZero.Domain.Common.csproj
-dotnet build code/IterationZero.Domain.Common/IterationZero.Domain.Common.csproj
-
-dotnet restore tests/IterationZero.Domain.Common.Tests.Unit/IterationZero.Domain.Common.Tests.Unit.csproj
-dotnet build tests/IterationZero.Domain.Common.Tests.Unit/IterationZero.Domain.Common.Tests.Unit.csproj
+dotnet restore IterationZero.Domain.Common.sln
+dotnet build IterationZero.Domain.Common.sln
 
 dotnet test tests/IterationZero.Domain.Common.Tests.Unit/IterationZero.Domain.Common.Tests.Unit.csproj
